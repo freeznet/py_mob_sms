9fef88f33ceec4d2cb7d6d957b5e4ac8f0e9b878
Rui Fu <freelz@gmail.com>
2015-02-12 11:33:35 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+#Mob SMS 短信服务端验证接口
+
+[http://wiki.mob.com/smssdk-service-verify/](http://wiki.mob.com/smssdk-service-verify/)
---
+++ mobsms.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import requests
+
+__author__ = 'rui'
+
+
+class MobSMS:
+    def __init__(self, appkey):
+        self.appkey = appkey
+        self.verify_url = 'https://api.sms.mob.com:8443/sms/verify'
+
+    def verify_sms_code(self, zone, phone, code, debug=False):
+        if debug:
+            return 200
+
+        data = {'appkey': self.appkey, 'phone': phone, 'zone': zone, 'code': code}
+        req = requests.post(self.verify_url, data=data, verify=False)
+        if req.status_code == 200:
+            j = req.json()
+            return j.get('status', 500)
+
+        return 500
+
+
+if __name__ == '__main__':
+    mobsms = MobSMS('you_key_gose_here')
+    print mobsms.verify_sms_code(86, 13900000000, '1234')
